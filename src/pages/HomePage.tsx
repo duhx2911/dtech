9425eb8897bca3fd841c7cb9973a38d9be484202
duhx2911 <nxd2911@gmail.com>
2023-12-16 20:50:28 +0700

    test payment

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,8 @@ import store from "../stores";
 import { getIpadList, getIphoneList } from "../stores/actions/productAction";
 import { useEffect } from "react";
 import ListIpad from "../components/pages/homepage/ListIpad";
+import { Button } from "antd";
+import axios from "axios";
 
 const HomePage = () => {
   const fetchData = async () => {
@@ -18,6 +20,15 @@ const HomePage = () => {
   }, []);
   return (
     <>
+      <Button
+        onClick={() => {
+          axios.post("http://localhost:8888/order/create_payment_url", {
+            amount: 120000,
+          });
+        }}
+      >
+        Thanh toán
+      </Button>
       <TopBanner />
       <NewProducts />
       <Hotdeal />
